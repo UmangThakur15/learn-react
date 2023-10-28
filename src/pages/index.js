16593ec6ddd112d554d473a0bf6153a724257eae
UmangThakur15 <umangthakuremail@gmail.com>
2023-10-27 20:19:22 -0400

    First Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile.js'
 import Profile_m from './qcomps/profile_mistake.js'
+import First_comp from './qcomps/firstcomp.js'
 
 export default function Home() {
   return (
     <div className={styles.main}>
         <Profile />
         <Profile_m />
+        <First_comp />
     </div>
   )
 }
